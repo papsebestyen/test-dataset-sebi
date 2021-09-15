4e41e7caa705146adbadf50d2ccb06897f56261c
papsebestyen <papsebestyen>
2021-09-15 15:38:00 +0200

    add src

--- src/create_subsets.py
+++ src/create_subsets.py
@@ -1,3 +1,17 @@
-def create_subsets(subset_name, **kwargs):
+from sscutils import dump_dfs_to_trepos
+
+from .trepos import karg_table
+from .raw_cols import CommonCols, EventsCols
+
+def create_subsets(subset_name, person_ids=None, only_success=False):
     """create subsets that are described in the config of the repo"""
-    pass
+    karg_df = karg_table.get_full_df().loc[lambda df: df['time'].apply(lambda x: x.strip().split('-')[0] == '2020')]
+    # pers_df = persons_table.get_full_df().loc[person_ids or slice(None), :]
+    # ev_df = events_table.get_full_df().loc[
+    #     lambda df: df[CommonCols.person_id].isin(pers_df.index)
+    #     & (df[EventsCols.success] | (not only_success)),
+    #     :,
+    # ]
+    # obj_df = objects_table.get_full_df().loc[ev_df[CommonCols.object_id].unique(), :]
+
+    dump_dfs_to_trepos(subset_name, [(karg_df, karg_table)])
--- src/raw_cols.py
+++ src/raw_cols.py
@@ -2,4 +2,18 @@ from colassigner import ColAccessor
 
 
 class CommonCols(ColAccessor):
-    obj_id = "object_id"
+    person_id = "person_id"
+    object_id = "object_id"
+
+
+class PersonsCols(ColAccessor):
+    name = "name"
+    age = "age"
+
+
+class ObjectsCols(ColAccessor):
+    size = "size"
+
+
+class EventsCols(ColAccessor):
+    success = "success"
